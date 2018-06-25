bd5259d84fd6a94fcfedd11b05e1b6c83585520f
sergiobianchi <sbianchi@mygestion.com.ar>
2018-06-25 19:58:27 -0300

    Agregado Indice On branch master

---
+++ ahorcado/js/index.js
@@ -0,0 +1,22 @@
+let partidasJugadas = 0, palabrasAdivinadas = 0, palabraSeleccionada = '', turnosRestantes = 0, letrasIngresadas = []
+
+function Inciiar(){
+
+}
+
+function seleccionarPalabra(){
+
+}
+
+function ingresoCaracter(){
+
+}
+
+function caracterValido(){
+
+}
+
+function caracterEnPalabra(){
+
+}
+
