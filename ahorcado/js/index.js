9d09856768616f351d0c359c58f32048162efaaa
sergiobianchi <sbianchi@mygestion.com.ar>
2018-06-25 20:36:00 -0300

    Funciones agregadas al index.js   Please enter the commit message for your changes. Lines starting

--- ahorcado/js/index.js
+++ ahorcado/js/index.js
@@ -1,22 +1,88 @@
-let partidasJugadas = 0, palabrasAdivinadas = 0, palabraSeleccionada = '', turnosRestantes = 0, letrasIngresadas = []
+let partidasJugadas, palabrasAdivinadas, palabraSeleccionada, turnosRestantes, letrasIngresadas = []
 
-function Inciiar(){
+function inicializar(){
+    partidasJugadas = 0;
+    palabrasAdivinadas = 0;
+}
+
+
+function seleccionarPalabra(palabras){
+    return palabras[Math.floor(Math.random() * palabras.length)];
+}
+
+function caracterValido(caracter){
 
 }
 
-function seleccionarPalabra(){
+function caracterEnPalabra(caracter){
+    return true;
+}
+
+function caracterEnCuadro(caracter){
+    return true;
+}
+
+function mostrarCaracterEnPalabra(caracter){
+
+}
+
+function mostrarCaracterEnCuadro(caracter){
+
+}
+
+function mostrarCartel(ganador){
+    if (ganador){
+
+    } else {
+
+    }
+
+}
+
+function gano(){
 
 }
 
 function ingresoCaracter(){
+    const caracter = '';
 
+    if (!caracterValido(caracter)) {
+        return ;
+    }
+
+    if (caracterEnCuadro(caracter)){
+        return;
+    }
+
+    if (caracterEnPalabra(caracter)){
+        mostrarCaracterEnPalabra(caracter);
+    } else {
+        letrasIngresadas.push(caracter);
+
+        mostrarCaracterEnCuadro(caracter);    
+    }
+
+    turnosRestantes--;
+
+    if (gano()) {
+        mostrarCartel(true);
+    } else {
+        if (turnosRestantes === 0){
+            mostrarCartel(false);
+        }
+    }
 }
 
-function caracterValido(){
+function jugar(){
+    partidasJugadas++
 
+    palabraSeleccionada = seleccionarPalabra(listado);
+     
+    turnosRestantes = palabraSeleccionada.length + 1;
+    letrasIngresadas = []
 }
 
-function caracterEnPalabra(){
-
-}
+window.onload = function() {
+    inicializar();
+  };
 
