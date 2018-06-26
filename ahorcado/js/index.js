1631fdcfb8fa13713285fe6be3fda57cdd67f37b
sergiobianchi <sbianchi@mygestion.com.ar>
2018-06-26 09:11:38 -0300

    Se incorporaron funciones en index.js :

--- ahorcado/js/index.js
+++ ahorcado/js/index.js
@@ -1,23 +1,31 @@
 let partidasJugadas, palabrasAdivinadas, palabraSeleccionada, turnosRestantes, letrasIngresadas, palabraAdivinada;
-const grillaLetras = document.getElementById("");
+const grillaLetras = document.getElementById("letrasIngresadas");
 const grillaPalabra = document.getElementById("espacioDeLetras");
 
 function inicializar(){
+    // Incializar variables Globales
+
     partidasJugadas = 0;
     palabrasAdivinadas = 0;
 }
 
 function seleccionarPalabra(palabras){
+    // Seleccionar Palabra aleatoria de un array
+
     return palabras[Math.floor(Math.random() * palabras.length)];
 }
 
 function caracterValido(caracter){
+    // Validar que el caracter introducido sea solo letra
+
     if( caracter == null || caracter.length == 0 || /^\s+$/.test(caracter) ) {
         return false;
-      }
     }
+}
 
 function caracterEnPalabra(caracter, palabra, adivinada){
+    // Verificar que la letra seleccionada este en la palabra a adivinar
+
     let estaEnPalabra = false;
 
     for (let i=0; i < palabra.length; i++){
@@ -31,10 +39,14 @@ function caracterEnPalabra(caracter, palabra, adivinada){
 }
 
 function caracterEnCuadro(caracter){
+    // Verificar que la letra seleccionada este en las letras que ya se usaron y no forman parte de la palabra a adivinar
+
     return true;
 }
 
 function mostrarCaracterEnPalabra(grilla, palabra){
+    // Muestra el estado de la palabra a adivinar en la pantalla
+
     const letrasEnPantalla = document.getElementsByClassName("guiones");
 
     for (let i = 0; i < palabra.length; i++){
@@ -43,10 +55,12 @@ function mostrarCaracterEnPalabra(grilla, palabra){
 }
 
 function mostrarCaracterEnCuadro(caracter){
+    // Muestra la letra seleccionada y que no forma parte de la palabra a adivinar en la pantalla
 
 }
 
 function mostrarCartel(ganador){
+    // Muestra lso carteles de final de juego, tanto para Ganador como para Perdedorpantalla
     if (ganador){
 
     } else {
@@ -55,11 +69,14 @@ function mostrarCartel(ganador){
 
 }
 
-function gano(){
+function esGanador(){
+    // Verifica si acerto la palabra
 
 }
 
 function sacarAcentos(palabra){
+    // Reemplaza las vocales acentuadas por vocales sin acento para las comparaciones
+
     let nuevaPalabra = '';
     
     for (let i=0; i < palabra.length; i++){
@@ -95,7 +112,9 @@ function sacarAcentos(palabra){
     return nuevaPalabra;
 }
 
-function ingresoCaracter(){
+function procesarCaracterIngresado(){
+    // Procesa el caracter ingresado por el usuario
+    
     const caracter = '';
 
     if (!caracterValido(caracter)) {
@@ -116,7 +135,7 @@ function ingresoCaracter(){
 
     turnosRestantes--;
 
-    if (gano()) {
+    if (esGanador()) {
         mostrarCartel(true);
     } else {
         if (turnosRestantes === 0){
@@ -125,7 +144,19 @@ function ingresoCaracter(){
     }
 }
 
-function crearGrillaPalabra(objetoGrilla, cantidadLetras){
+function blanquearGrilla(grilla){
+    // Elimina los elementos que pudiera tener una grilla de la partida anterio, si huboestado de la palabra a adivinar
+
+    while (grilla.firstChild) {
+        grilla.removeChild(grilla.firstChild);
+    }
+}
+
+function inicializarGrillaPalabra(objetoGrilla, cantidadLetras){
+    // Configura la grilla donde se mostrara el estado de la palabra a adivinar
+
+    blanquearGrilla(objetoGrilla);
+
     for (let i = 0; i < cantidadLetras; i++){
         const letra = document.createElement("div");
         letra.className = "guiones";
@@ -134,20 +165,27 @@ function crearGrillaPalabra(objetoGrilla, cantidadLetras){
     }
 }
 
+function inicializarGrillaLetras(objetoGrilla){
+    // Configura la grilla donde se mostrara las letras ingresadas y que no forman parte de la letra a adivinar
+
+    blanquearGrilla(objetoGrilla);    
+}
+
 function jugar(){
+    // Lanza el juego e inicializar lo necesario para la nueva partida
+
     partidasJugadas++
 
-    palabraSeleccionada = seleccionarPalabra(listado);
-    palabraSeleccionada = sacarAcentos(palabraSeleccionada);
+    palabraSeleccionada = sacarAcentos(seleccionarPalabra(listado));
 
     for (let i=0; i < palabraSeleccionada.length; i++){
         palabraAdivinada.push('_');
     }
 
-    crearGrillaPalabra(grillaPalabra, palabraSeleccionada.length);
+    inicializarGrillaPalabra(grillaPalabra, palabraSeleccionada.length);
 
-    mostrarCaracterEnPalabra(grillaPalabra, palabraAdivinada)
-     
+    inicializarGrillaLetras(grillaLetras);
+
     turnosRestantes = palabraSeleccionada.length + 1;
     letrasIngresadas = []
 }
