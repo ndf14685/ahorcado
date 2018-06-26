66cf1e92c60693085ac1673758e1363f6d7763e1
sergiobianchi <sbianchi@mygestion.com.ar>
2018-06-25 21:41:08 -0300

    Actualizado de funciones

--- ahorcado/js/index.js
+++ ahorcado/js/index.js
@@ -1,29 +1,45 @@
-let partidasJugadas, palabrasAdivinadas, palabraSeleccionada, turnosRestantes, letrasIngresadas = []
+let partidasJugadas, palabrasAdivinadas, palabraSeleccionada, turnosRestantes, letrasIngresadas, palabraAdivinada;
+const grillaLetras = document.getElementById("");
+const grillaPalabra = document.getElementById("espacioDeLetras");
 
 function inicializar(){
     partidasJugadas = 0;
     palabrasAdivinadas = 0;
 }
 
-
 function seleccionarPalabra(palabras){
     return palabras[Math.floor(Math.random() * palabras.length)];
 }
 
 function caracterValido(caracter){
+    if( caracter == null || caracter.length == 0 || /^\s+$/.test(caracter) ) {
+        return false;
+      }
+    }
 
-}
+function caracterEnPalabra(caracter, palabra, adivinada){
+    let estaEnPalabra = false;
 
-function caracterEnPalabra(caracter){
-    return true;
+    for (let i=0; i < palabra.length; i++){
+        if (caracter.toUpperCase() === palabra[i].toUpperCase()){
+            adivinada[i] = palabra[i].toUpperCase();
+            estaEnPalabra = true;
+        }
+    }
+
+    return estaEnPalabra;
 }
 
 function caracterEnCuadro(caracter){
     return true;
 }
 
-function mostrarCaracterEnPalabra(caracter){
+function mostrarCaracterEnPalabra(grilla, palabra){
+    const letrasEnPantalla = document.getElementsByClassName("guiones");
 
+    for (let i = 0; i < palabra.length; i++){
+        letrasEnPantalla[i].textContent = palabra[i];
+    }
 }
 
 function mostrarCaracterEnCuadro(caracter){
@@ -43,6 +59,42 @@ function gano(){
 
 }
 
+function sacarAcentos(palabra){
+    let nuevaPalabra = '';
+    
+    for (let i=0; i < palabra.length; i++){
+        let letra = palabra[i].toUpperCase();
+
+        switch ( letra ) {
+            case "Á": 
+                letra = "A";
+                break;
+    
+            case "É":
+                letra = "E";                        
+                break;
+
+            case "Í": 
+                letra = "I";
+                break;
+    
+            case "Ó":
+                letra = "O";                        
+                break;
+
+            case "Ú":
+                letra = "U";                        
+                break;
+
+            default:
+        }        
+
+        nuevaPalabra += letra;
+    }
+
+    return nuevaPalabra;
+}
+
 function ingresoCaracter(){
     const caracter = '';
 
@@ -54,8 +106,8 @@ function ingresoCaracter(){
         return;
     }
 
-    if (caracterEnPalabra(caracter)){
-        mostrarCaracterEnPalabra(caracter);
+    if (caracterEnPalabra(caracter, palabraSeleccionada, palabraAdivinada)){
+        mostrarCaracterEnPalabra(grillaPalabra, palabraAdivinada);
     } else {
         letrasIngresadas.push(caracter);
 
@@ -73,10 +125,28 @@ function ingresoCaracter(){
     }
 }
 
+function crearGrillaPalabra(objetoGrilla, cantidadLetras){
+    for (let i = 0; i < cantidadLetras; i++){
+        const letra = document.createElement("div");
+        letra.className = "guiones";
+        letra.textContent = "_";
+        objetoGrilla.appendChild(letra);
+    }
+}
+
 function jugar(){
     partidasJugadas++
 
     palabraSeleccionada = seleccionarPalabra(listado);
+    palabraSeleccionada = sacarAcentos(palabraSeleccionada);
+
+    for (let i=0; i < palabraSeleccionada.length; i++){
+        palabraAdivinada.push('_');
+    }
+
+    crearGrillaPalabra(grillaPalabra, palabraSeleccionada.length);
+
+    mostrarCaracterEnPalabra(grillaPalabra, palabraAdivinada)
      
     turnosRestantes = palabraSeleccionada.length + 1;
     letrasIngresadas = []
